e18ab3fb9cc16d25a653434cd56cadc89c88d5c4
AlanYao08 <AlanYao08@users.noreply.github.com>
2022-06-14 03:28:23 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -8,11 +8,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j=0; j<width; j++)
         {
-            RGBTRIPLE pixel = image[i][j];
-            int average = round((pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3.);
-            pixel.rgbtBlue = average;
-            pixel.rgbtGreen = average;
-            pixel.rgbtRed = average;
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.);
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
         }
     }
 }
