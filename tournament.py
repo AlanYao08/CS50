f6b52e68f9c26affed6d5bb64b53df2bcbd85f31
AlanYao08 <AlanYao08@users.noreply.github.com>
2022-06-16 22:36:18 +0000

    automated commit by submit50

--- tournament.py
+++ tournament.py
@@ -7,6 +7,7 @@ import random
 # Number of simluations to run
 N = 1000
 
+
 def main():
 
     # Ensure correct usage
@@ -36,12 +37,14 @@ def main():
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
+
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
     rating1 = team1["rating"]
     rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
+
 
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
@@ -56,12 +59,14 @@ def simulate_round(teams):
 
     return winners
 
+
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     while(len(teams) != 1):
         teams = simulate_round(teams)
 
     return teams[0]["team"]
+
 
 if __name__ == "__main__":
     main()
